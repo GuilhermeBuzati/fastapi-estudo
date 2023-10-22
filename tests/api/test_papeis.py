c1124115af884b131211398d03489e0b67e583ee
guilherme <guilhermeleopoldinoo@gmail.com>
2023-10-22 18:10:30 -0300

    feat: Aula 07 - Adicionando testes

---
+++ tests/api/test_papeis.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+from tests.utils.papeis import create_papel_valido
+
+
+def test_cria_papel(client: TestClient) -> None:
+    body = create_papel_valido()
+
+    response = client.post("/papeis/", json=body)
+
+    content = response.json()
+
+    assert response.status_code == 200
+    assert content['cnpj'] == body['cnpj']
